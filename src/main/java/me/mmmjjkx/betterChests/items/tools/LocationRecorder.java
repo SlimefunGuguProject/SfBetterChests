76793a37d72bcc2c7fc9a61c829aefbce04d86e1
haiman <83174104+haiman233@users.noreply.github.com>
2025-01-03 17:27:19 +0800

    汉化

--- src/main/java/me/mmmjjkx/betterChests/items/tools/LocationRecorder.java
+++ src/main/java/me/mmmjjkx/betterChests/items/tools/LocationRecorder.java
@@ -92,7 +92,7 @@ public class LocationRecorder extends SimpleSlimefunItem<ItemUseHandler> impleme
                     Location loc = new Location(Bukkit.getWorld(world.get()), x.getAsInt(), y.getAsInt(), z.getAsInt());
 
                     if (!Slimefun.getProtectionManager().hasPermission(p, loc, Interaction.INTERACT_BLOCK)) {
-                        p.sendMessage("§cYou do not have permission to access this location.");
+                        p.sendMessage("§c您没有权限访问此位置。");
                         return;
                     }
 
@@ -101,14 +101,14 @@ public class LocationRecorder extends SimpleSlimefunItem<ItemUseHandler> impleme
 
                         SlimefunItem item = BlockStorage.check(loc);
                         if (item != null && item.getId().equals("NTW_GRID")) {
-                            p.sendMessage("§cYou cannot record a location of a Networks Grid.");
+                            p.sendMessage("§c您不能记录网络网格的位置。");
                             return; // Networks already had remote accessor
                         }
 
                         menu.open(p);
                     }
                 } else {
-                    p.sendMessage("§cYou haven't recorded a location yet.");
+                    p.sendMessage("§c您尚未记录位置。");
                 }
             }
         };
